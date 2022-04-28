95c032bf4d38e79208b875ea2652d0817deb6df0
Mario Meza <78285858+Leojr2021@users.noreply.github.com>
2022-04-28 11:40:11 -0500

    Autogenera  datos en el BD cada vez que se levanta el server

---
+++ api/json/products.js
@@ -0,0 +1,84 @@
+const { Product } = require("../src/db");
+const productMockUp = async () => {
+  try {
+    await Product.create({
+      name: "Headphones",
+      price: 300,
+      // newItem: false,
+      brand: "Samsung",
+      color: "red",
+      warranty: 3,
+      discount: 2,
+      image: [
+        "https://picsum.photos/300",
+        "https://picsum.photos/300",
+      ],
+      stock: 50,
+      description:'asdasfasgfagasfgasfasfasfasfsafas',
+      // rating: 0,
+      brand: 1,
+      categoryId: 1,
+    });
+    await Product.create({
+      name: "PC MASTER",
+      price: 500,
+      // newItem: false,
+      brand: "Samsung",
+      color: "BLACK",
+      warranty: 1,
+      discount: 30,
+      image: [
+        "https://picsum.photos/300",
+        "https://picsum.photos/300",
+      ],
+      stock: 20,
+      description:'54654894131646431264',
+      // rating: 0,
+      brand: 1,
+      categoryId: 2,
+    });
+
+    await Product.create({
+      name: "Dog",
+      price: 500,
+      // newItem: false,
+      brand: "pepito",
+      color: "BLACK",
+      warranty: 1,
+      discount: 30,
+      image: [
+        "https://picsum.photos/300",
+        "https://picsum.photos/300",
+      ],
+      stock: 20,
+      description:'hermoso regalo para tu perro',
+      // rating: 0,
+      brand: 1,
+      categoryId: 3,
+    });
+
+    await Product.create({
+      name: "Espejo",
+      price: 500,
+      // newItem: false,
+      brand: "Generica",
+      color: "BLACK",
+      warranty: 1,
+      discount: 30,
+      image: [
+        "https://picsum.photos/300",
+        "https://picsum.photos/300",
+      ],
+      stock: 20,
+      description:'Espejo con zoom',
+      // rating: 0,
+      brand: 1,
+      categoryId: 4,
+    });
+   
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+module.exports = productMockUp;
