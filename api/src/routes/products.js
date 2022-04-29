d94c61b60d9f4fdd569620ff5bf0ead2c6881e39
Johannes Gómez <87533958+JohannesGomez@users.noreply.github.com>
2022-04-29 09:12:39 -0400

    Modificacion tabla producto

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -13,22 +13,46 @@ const {Op} = require('sequelize');
      - Si no existe ninguna raza de perro mostrar un mensaje adecuado
 */
 router.get('/', async (req, res, next) => {
-  console.log('Estoy BACK get __GET /products__ ',req.query)
+  //console.log('Estoy BACK get __GET /products__ ',req.query)
   let {name} = req.query
     try{
        if(!name){ // All records table if name is empty
          let getAllBdProduct = await Product.findAll({
              include: [Category, Reviews] // name de la categoria relacionado a su categoryId
          }) 
-         return res.status(200).send(getAllBdProduct);
+         return res.status(200).send(await getAllProduct(getAllBdProduct));
       }else { // Filter by name, name is not empty
+        console.log('Estoy BACK get __GET /products Name ',req.query)
          let getAllBdProduct = await Product.findAll({
              include: [Category, Reviews], // name de la categoria relacionado a su categoryId
              where:{name: {[Op.iLike] : `${name}%`}} // Filtra por name product
          }) 
-         return res.status(200).send(getAllBdProduct);        
+         //console.log(getAllBdProduct)
+         return res.send(await getAllProduct(getAllBdProduct));
       }     
       }catch(error) {next(error)}
 })
+
+const getAllProduct = async (getAllBdProduct) => {
+  const getAllBdProduct1 = getAllBdProduct.map((elem) =>{
+    return {
+      id          : elem.id,   
+      description : elem.description,
+      name        : elem.name,
+      image       : elem.image,    
+      price       : elem.price,   
+      brand       : elem.brand,   
+      genres      : elem.genre, 
+      color       : elem.color, 
+      discount    : elem.discount,
+      stock       : elem.stock,
+      rating      : elem.rating,
+      warranty    : elem.warranty,
+      category    : elem.category.name,
+      reviews : elem.reviews.map(ele => ele.rating)
+  }})                
+  return(getAllBdProduct1)
+  
+}
 
 module.exports = router;
