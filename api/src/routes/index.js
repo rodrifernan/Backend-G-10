e3d5ddaa5f1f6eef8472ef9c0f2057daf61dbc11
Johannes Gómez <87533958+JohannesGomez@users.noreply.github.com>
2022-05-01 20:23:08 -0400

    Creacion Ruta POST Category

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,17 +1,15 @@
-const { Router }  = require('express');
-const products = require('./products');  // ruta get     
-const categories = require('./categories');  // ruta get     
-const product  = require('./product');   // ruta post    
-const user = require('./user');
+const { Router }     = require('express');
+const products       = require('./products');  
+const category       = require('./category');
+const categories     = require('./categories');
+const product        = require('./product');   
+const user           = require('./user');
 const filtercategory = require('./filtercategory')
-const genresFilter = require('./genre');
-const login = require('./login');
-const shoppingCart = require('./shoppingCart');
-const wishlist = require('./wishlist');
-const productById = require('./productById');
-
-
-
+const genresFilter   = require('./genre');
+const login          = require('./login');
+const shoppingCart   = require('./shoppingCart');
+const wishlist       = require('./wishlist');
+const productById    = require('./productById');
 
 // mildelword
 const router = Router();
@@ -21,6 +19,7 @@ const router = Router();
 router.use('/products', products); 
 router.use('/product', product); 
 router.use('/categories',categories); 
+router.use('/category',category); 
 router.use('/user', user); 
 router.use('/filtercategory', filtercategory); 
 router.use('/genresFilter', genresFilter); 
