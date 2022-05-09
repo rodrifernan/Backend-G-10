99cde176c575e7fcfecb22f14d9c6f9ceb54b02d
lalechelml <ramos.joshelin@hotmail.com>
2022-05-08 21:54:52 -0500

    mercado pago prueba

--- api/json/shoppingCart.js
+++ api/json/shoppingCart.js
@@ -4,25 +4,25 @@ const shoppingCartMockUp = async () => {
     await ShoppingCart.create({
       userId: '69cacb3c-4ef3-4d72-bbf3-d6618e45a45c',
       productId: '4841011e-fbf5-4c92-a770-3b06684f5892',
-      quantity: 2,
-    });
-    
-    await ShoppingCart.create({
-      userId: '69cacb3c-4ef3-4d72-bbf3-d6618e45a45b',
-      productId: '4841011e-fbf5-4c92-a770-3b06684f5892',
       quantity: 1,
     });
 
     await ShoppingCart.create({
       userId: '69cacb3c-4ef3-4d72-bbf3-d6618e45a45c',
-      productId: '4841011e-fbf5-4c92-a770-3b06684f5892',
-      quantity: 5,
+      productId: '4841011e-fbf5-4c92-a770-3b06684f5891',
+      quantity: 1,
     });
 
     await ShoppingCart.create({
       userId: '69cacb3c-4ef3-4d72-bbf3-d6618e45a45b',
-      productId: '4841011e-fbf5-4c92-a770-3b06684f5892',
-      quantity: 7,
+      productId: '4841011e-fbf5-4c92-a770-3b06684f5893',
+      quantity: 1,
+    });
+
+    await ShoppingCart.create({
+      userId: '69cacb3c-4ef3-4d72-bbf3-d6618e45a45b',
+      productId: '4841011e-fbf5-4c92-a770-3b06684f5891',
+      quantity: 1,
     });
   } catch (err) {
     console.log(err.message);
