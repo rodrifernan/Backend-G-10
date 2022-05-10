a75b657d9e797364b8dfa32b0f99ffbc4885b9cb
M0n00s <programacion.buffalo@gmail.com>
2022-05-10 17:40:09 -0400

    add stock in product post

--- api/src/db.js
+++ api/src/db.js
@@ -1,94 +1,83 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
-const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
-} = process.env;
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ecommerce`, {
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
 
-
 let sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-        database: DB_NAME,
-        dialect: "postgres",
-        host: DB_HOST,
-        port: 5432,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000,
-        },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            // Ref.: https://github.com/brianc/node-postgres/issues/2009
-            rejectUnauthorized: false,
-          },
-          keepAlive: true,
-        },
-        ssl: true,
-      })
-    : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ecommerce`,
-        { logging: false, native: false }
-      );
-
-
-
-
-
+	process.env.NODE_ENV === "production"
+		? new Sequelize({
+				database: DB_NAME,
+				dialect: "postgres",
+				host: DB_HOST,
+				port: 5432,
+				username: DB_USER,
+				password: DB_PASSWORD,
+				pool: {
+					max: 3,
+					min: 1,
+					idle: 10000,
+				},
+				dialectOptions: {
+					ssl: {
+						require: true,
+						// Ref.: https://github.com/brianc/node-postgres/issues/2009
+						rejectUnauthorized: false,
+					},
+					keepAlive: true,
+				},
+				ssl: true,
+		  })
+		: new Sequelize(
+				`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ecommerce`,
+				{ logging: false, native: false }
+		  );
 
 // *****************************
-
-
-
-
-
-
-
-
 
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
-
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
-  .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
-  });
+fs.readdirSync(path.join(__dirname, "/models"))
+	.filter(
+		(file) =>
+			file.indexOf(".") !== 0 &&
+			file !== basename &&
+			file.slice(-3) === ".js"
+	)
+	.forEach((file) => {
+		modelDefiners.push(require(path.join(__dirname, "/models", file)));
+	});
 
 // Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach(model => model(sequelize));
+modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+let capsEntries = entries.map((entry) => [
+	entry[0][0].toUpperCase() + entry[0].slice(1),
+	entry[1],
+]);
 sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { 
-  
-  User,
-  Role,
-  ShoppingCart,
-  Wishlist,
-  Product,
-  Category,
-  Order,
-  Reviews,
-  Genre
-
+const {
+	User,
+	Role,
+	ShoppingCart,
+	Wishlist,
+	Product,
+	Category,
+	Order,
+	Reviews,
+	Genre,
 } = sequelize.models;
 
 // Aca vendrian las relaciones
@@ -107,7 +96,6 @@ Order.belongsTo(Product);
 Category.hasMany(Product);
 Product.belongsTo(Category);
 
-
 // //genre/product 1-*
 Genre.hasMany(Product);
 Product.belongsTo(Genre);
@@ -118,28 +106,26 @@ User.hasMany(Order);
 Order.belongsTo(User);
 
 // //User/ShoppingCart 1-*
- User.hasOne(ShoppingCart);
- ShoppingCart.belongsTo(User);
+User.hasOne(ShoppingCart);
+ShoppingCart.belongsTo(User);
 
 // //User/ShoppingCart 1-*
- Product.hasMany(ShoppingCart);
- ShoppingCart.belongsTo(Product);
+Product.hasMany(ShoppingCart);
+ShoppingCart.belongsTo(Product);
 
 // // //product/review
-   Product.hasMany(Reviews);
-   Reviews.belongsTo(Product);
+Product.hasMany(Reviews);
+Reviews.belongsTo(Product);
 
 // // // //user/product *-*
-   User.hasOne(Wishlist);
-   Wishlist.belongsTo(User);
+User.hasOne(Wishlist);
+Wishlist.belongsTo(User);
 
 // // //wishlist/product *-*
 //   Product.hasMany(Wishlist);
 //   Wishlist.belongsTo(Product);
 
-
-
 module.exports = {
-  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+	...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+	conn: sequelize, // para importart la conexión { conn } = require('./db.js');
 };
