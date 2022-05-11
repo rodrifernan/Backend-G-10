41919d7a356ebceaa53e4a4b9a086361b5367b3b
lalechelml <ramos.joshelin20@gmail.com>
2022-05-11 14:02:12 -0500

    order user details / user product

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
-const {Router} = require('express');
-const {Product} = require('../db') // traer mi modelo
+const { Router } = require('express');
+const { Product } = require('../db'); // traer mi modelo
 const router = Router();
+const { loginVerification } = require('../middlewares/login');
 //const {} = require('./utiles');
 
 /* #### Backend 
@@ -9,17 +10,44 @@ const router = Router();
     - Recibe los datos recolectados desde el formulario controlado de la ruta
       de creación del producto por body
     - Crea un producto en la base de datos */
-router.post('/', async (req, res, next) => {
-      console.log('estoy BACK POST /product', req.body)
-      let {name,description,price,brand,color,warranty,image,discount,stock,categoryId,genreId} = req.body
-      try {   
-          let postBdProduct = await Product.create({
-              name,description,price,brand,color,warranty,image,discount
-          })
-          //return res.send(await postBdProduct.setCategory(categoryId));
-          return res.status(200).send(postBdProduct); 
-    }catch(error) {next(error)}
-})
+router.post('/', loginVerification, async (req, res, next) => {
+  console.log('estoy BACK POST /product', req.body);
+
+  const { id: userId } = req.user;
+
+  let {
+    name,
+    description,
+    price,
+    brand,
+    color,
+    warranty,
+    image,
+    discount,
+    stock,
+    categoryId,
+    genreId,
+  } = req.body;
+  try {
+    let postBdProduct = await Product.create({
+      name,
+      description,
+      price,
+      brand,
+      color,
+      warranty,
+      image,
+      discount,
+      stock,
+      categoryId,
+      genreId,
+      userId,
+    });
+    //return res.send(await postBdProduct.setCategory(categoryId));
+    return res.status(200).send(postBdProduct);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
-
