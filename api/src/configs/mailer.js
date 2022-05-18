2f8526d733e0e621e740d7fb45934486b3c34548
leidydi621 <leidydi621@gmail.com>
2022-05-18 08:25:38 -0500

    actualizacion de direccion email

--- api/src/configs/mailer.js
+++ api/src/configs/mailer.js
@@ -1,14 +1,15 @@
 const nodemailer = require('nodemailer');
 
  const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, 
-    auth: {
-      user: "efdwxstkn5itdeg6@ethereal.email", 
-      pass: "ucwbfQRVgf2D1pEDC3", 
-    },
-  });
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, 
+  auth: { // nombre de usuario y nuestra contraseña
+  user: "johannes.gomez@gmail.com", // correo de host
+  pass: "mdiqfuqrtfvrhefn",          // contraseña generada de google de nuestra aplicacion
+  },
+  tls: {rejectUnauthorized:false}
+});
 transporter.verify().then(() => {
   console.log('Server is ready for send emails');
 })
